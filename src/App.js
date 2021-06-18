899417f415c2a3414edb6584524090f1756866b4
cygnu <crucia8@icloud.com>
2021-06-19 01:34:01 +0900

    solved station7

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // DO NOT DELETE
 
 import * as React from 'react'
+import './reset.css'
 import './App.css'
 
 /**
@@ -8,26 +9,33 @@ import './App.css'
  * @type {React.FC}
  */
 export const App = () => {
-  const initalState = "https://images.dog.ceo/breeds/akita/Akita_inu_blanc.jpg";
+  const initialState = "https://images.dog.ceo/breeds/akita/Akita_inu_blanc.jpg";
 
-  const [docUrl, setDocUrl] = React.useState(initalState);
+  const [docUrl, setDocUrl] = React.useState(initialState);
 
   return (
-    <header>
-      <h1>Dogアプリ</h1>
-      <p>犬の画像を表示するサイトです。</p>
-      <img src={docUrl} />
-      <button
-        onClick={
-          () => fetch("https://dog.ceo/api/breeds/image/random")
-            .then(response => response.json())
-            .then((data) => {
-              if (data.status === "success") {
-                setDocUrl(data.message)
-              }
-            })
-        }
-      >更新</button>
-    </header>
+    <React.Fragment>
+      <header className="header">
+        <h1>Dogアプリ</h1>
+      </header>
+      <main className="container">
+        <p className="cTitle">犬の画像を表示するサイトです。</p>
+        <div className="cDoc">
+          <img src={docUrl} className="cDocImage" />
+          <button
+            className="cDocButton"
+            onClick={
+              () => fetch("https://dog.ceo/api/breeds/image/random")
+                .then(response => response.json())
+                .then((data) => {
+                  if (data.status === "success") {
+                    setDocUrl(data.message)
+                  }
+                })
+            }
+          >更新</button>
+        </div>
+      </main>
+    </React.Fragment>
   );
 }
